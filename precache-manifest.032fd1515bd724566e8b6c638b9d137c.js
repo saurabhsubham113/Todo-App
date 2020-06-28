3de7700ad9b538a468f584cd376912c28d7bc761
subham saurabh <saurabhsubham113@gmail.com>
2020-06-28 18:52:34 +0530

    Updates

---
+++ precache-manifest.032fd1515bd724566e8b6c638b9d137c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0c0510e4fb6a4475275e6e894cec8e",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "3abedcb9cc9ee70291e6",
+    "url": "/Todo-app/static/css/main.1703ea29.chunk.css"
+  },
+  {
+    "revision": "d2ab7237aea14fa88586",
+    "url": "/Todo-app/static/js/2.ad90caf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-app/static/js/2.ad90caf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3abedcb9cc9ee70291e6",
+    "url": "/Todo-app/static/js/main.024007ee.chunk.js"
+  },
+  {
+    "revision": "1eb6d6ec195e648776ea",
+    "url": "/Todo-app/static/js/runtime-main.375335ae.js"
+  }
+]);
